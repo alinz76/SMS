dabfa5e09ae74cb06e7f2054b60b3e7846397181
ali <ali.nezamzadeh@gmail.com>
2024-10-22 14:38:46 +0330

    some ui changes

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from urllib.parse import urlencode
 from base64 import b64encode
 from .forms import SMSForm
@@ -14,14 +15,18 @@ def home_page(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         idendity = f'{username}:{password}'
-        # userAndPass = urlencode(idendity)
         bytestr = bytes(idendity, encoding="ascii")
         userAndPass = b64encode(bytestr).decode("ascii")
         headers = { 'Authorization' : 'Basic %s' %  userAndPass , 'Content-type': 'application/json' , 'encoding':'utf-8'}
-        link = f'{url}AccessHash={request.POST.get('access_hash')}&PhoneNumber={request.POST.get('phone_number')}&PatternId={request.POST.get("pattern_id")}&RecNumber={request.POST.get("rec_number")}&SmsClass={request.POST.get("sms_class")}'
+        link = f'{url}AccessHash={request.POST.get('access_hash').strip()}&PhoneNumber={request.POST.get('phone_number')}&PatternId={request.POST.get("pattern_id").strip()}&RecNumber={request.POST.get("rec_number")}&SmsClass={request.POST.get("sms_class")}&token1={request.POST.get("token1")}'
         print(link)
         data = requests.get(link, headers=headers)
         response = data.text
         print(response)
+        if response:
+            if int(response) > 2000:
+                messages.success(request, 'با موفقیت ارسال شد')
+        else:
+            messages.error(request, "خطایی رخ داد")
         return render(request, 'app/homepage.html', context=context)
     return render(request, 'app/homepage.html', context=context)
